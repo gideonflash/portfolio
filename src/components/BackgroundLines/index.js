2f1121458f68357e090464eb2d58fa73d0de3a00
Gideon Kadzura <gideonkadzura@gmail.com>
2018-01-13 19:06:33 +0000

    Main app changes

--- src/components/BackgroundLines/index.js
+++ src/components/BackgroundLines/index.js
@@ -1,1 +1,99 @@
+import React from "react";
+import styled from "styled-components";
 
+const Line = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  left: 10%;
+  border: 0.5px solid #5499b8;
+`;
+const Line2 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 20%;
+  border: 0.5px solid #5499b8;
+`;
+const Line3 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 30%;
+  border: 0.5px solid #5499b8;
+`;
+const Line4 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 40%;
+  border: 0.5px solid #5499b8;
+`;
+const Line5 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 50%;
+  border: 0.5px solid #5499b8;
+`;
+const Line6 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 60%;
+  border: 0.5px solid #5499b8;
+`;
+const Line7 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 70%;
+  border: 0.5px solid #5499b8;
+`;
+const Line8 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 80%;
+  border: 0.5px solid #5499b8;
+`;
+const Line9 = styled.div`
+  position: fixed;
+  z-index: 0;
+  top: 28px;
+  bottom: 131px;
+  background-color: red;
+  left: 90%;
+  border: 0.5px solid #5499b8;
+`;
+
+const BackgroundLines = () => (
+  <div>
+    <Line />
+    <Line2 />
+    <Line3 />
+    <Line4 />
+    <Line5 />
+    <Line6 />
+    <Line7 />
+    <Line8 />
+    <Line9 />
+  </div>
+);
+
+export default BackgroundLines;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,87 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import MainPage from "../MainPage";
 import Footer from "../Footer";
-
-const Line = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  left: 10%;
-  border: 0.5px solid #5499b8;
-`;
-const Line2 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 20%;
-  border: 0.5px solid #5499b8;
-`;
-const Line3 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 30%;
-  border: 0.5px solid #5499b8;
-`;
-const Line4 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 40%;
-  border: 0.5px solid #5499b8;
-`;
-const Line5 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 50%;
-  border: 0.5px solid #5499b8;
-`;
-const Line6 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 60%;
-  border: 0.5px solid #5499b8;
-`;
-const Line7 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 70%;
-  border: 0.5px solid #5499b8;
-`;
-const Line8 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 80%;
-  border: 0.5px solid #5499b8;
-`;
-const Line9 = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 28px;
-  bottom: 131px;
-  background-color: red;
-  left: 90%;
-  border: 0.5px solid #5499b8;
-`;
+import BackgroundLines from "../../components/BackgroundLines";
 
 const AppWrapper = styled.div`
   margin: 28px;
@@ -93,15 +13,7 @@ const AppWrapper = styled.div`
 const Main = () => (
   <div>
     <AppWrapper>
-      <Line />
-      <Line2 />
-      <Line3 />
-      <Line4 />
-      <Line5 />
-      <Line6 />
-      <Line7 />
-      <Line8 />
-      <Line9 />
+      <BackgroundLines />
       <MainPage />
     </AppWrapper>
     <Footer />
