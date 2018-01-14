0c9dd950f6649b0fab4669cb6b5b88078e2146f8
Gideon Kadzura <gideonkadzura@gmail.com>
2018-01-14 15:52:16 +0000

    page folder section added

---
+++ src/containers/AshleenLesterPage/SectionThree/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  width: 1024px;
+  height: 100%;
+  margin-top: 86px;
+`;
+
+const SectionThree = () => <Section>section three</Section>;
+
+export default SectionThree;
---
+++ src/containers/VoguePayPage/SectionTwo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  width: 1024px;
+  height: 100%;
+  margin-top: 100px;
+`;
+
+const SectionTwo = () => <Section>section three</Section>;
+
+export default SectionTwo;
