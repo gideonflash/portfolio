57778ffca0304b7a27da699133aa1a19f6efe478
Gideon Kadzura <gideonkadzura@gmail.com>
2018-01-21 05:03:19 +0000

    Section About

--- src/containers/AboutPage/SectionTwo/index.js
+++ src/containers/AboutPage/SectionTwo/index.js
@@ -14,13 +14,85 @@ const Section = styled.div`
     margin-top: 80px;
   }
 `;
+const AboutContainer = styled.div`
+  display: grid;
+  grid-template-columns: 37.5% 50%;
+  @media (max-width: 420px) {
+    grid-template-columns: 1fr;
+  }
+`;
+const ProjectLine = styled.span`
+  display: inline-block;
+  background-color: #ffffff;
+  width: 80%;
+  height: 2px;
+  margin-left: 10px;
+  margin-bottom: 1px;
+  @media (max-width: 420px) {
+    height: 1px;
+    display: none;
+  }
+  @media only screen and (device-width: 768px) {
+    height: 1px;
+  }
+`;
+const ValuePropText = styled.h2`
+  font-family: Futura-Bold;
+  font-size: 36px;
+  color: #443d3d;
+  letter-spacing: 0;
+  font-weight: normal;
+  margin-top: 0px;
+  @media (max-width: 420px) {
+    font-size: 21px;
+  }
+  @media only screen and (device-width: 768px) {
+    font-size: 30px;
+  }
+`;
+const ProfileText = styled.p`
+  font-family: SanFranciscoText-Regular;
+  font-size: 16px;
+  color: #443d3d;
+  letter-spacing: 0;
+  @media (max-width: 420px) {
+    font-size: 13px;
+  }
+  @media only screen and (device-width: 768px) {
+    font-size: 16px;
+  }
+`;
 
 const SectionTwo = () => (
   <Section>
-    <div>
-      <div>line</div>
-      <div>1</div>
-    </div>
+    <AboutContainer>
+      <div>
+        <ProjectLine />
+      </div>
+      <div>
+        <ValuePropText>
+          I would like to say<br /> something cool <br /> so sound super smart
+        </ValuePropText>
+        <ProfileText>
+          Subject as I can. I have been driven by the need to understand,
+          testing my learning and trying as hard as I can to disprove evidence
+          that I think is true as a way of discovering. Adopting the mental
+          models Subject as I can. I have been driven by the need to understand,
+          testing my learning and trying as hard as I can to disprove evidence
+          that I think is true as a way of discovering. Adopting the mental
+          models
+          <br />
+          <br />
+          Subject as I can. I have been driven by the need to understand,
+          testing my learning and trying as hard as I can to disprove evidence
+          that I think is true as a way of discovering. Adopting the mental
+          models Subject as I can. I have been driven by the need to understand,
+          testing my learning and trying as hard as I can to disprove evidence
+          that I think is true as a way of discovering. Adopting the mental
+          models
+        </ProfileText>
+      </div>
+    </AboutContainer>
   </Section>
 );
 
